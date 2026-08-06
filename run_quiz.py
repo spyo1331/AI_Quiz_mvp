84d6c6fa554de8da73f563fd123d188f21db5ed6
unknown <TheMaksimkaX2007@yandex.ru>
2026-08-06 19:16:30 +0300

    Fixed port conflict

--- run_quiz.py
+++ run_quiz.py
@@ -1,6 +1,8 @@
 from flask_app import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.getenv('PORT', '8000'))
+    app.run(host='0.0.0.0', port=port, debug=False)
